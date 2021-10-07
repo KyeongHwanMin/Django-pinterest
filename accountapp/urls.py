71ac806b89a1b1bb580955596204e6ad38e1d4e8
KyeongHwanMin <h45652@naver.com>
2021-10-07 18:38:46 +0900

    feact(login.html) : login,logout 구현

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from accountapp.views import hello_world, AccountCreateView
 
@@ -5,7 +6,10 @@ app_name = "accountapp"
 
 #127.0.0.1:8000/account/hello_world == accountapp:hello_world"
 urlpatterns = [
-    path('hello_world/',hello_world,name='hello_world'),
-    #       라우터(주소),hello_world 함수, hello_world로 이름설정
+    path('hello_world/',hello_world,name='hello_world'),#       라우터(주소),hello_world 함수, hello_world로 이름설정
+
+    path('login/',LoginView.as_view(template_name='accountapp/login.html'),name='login'), # 로그인 view는 템플릿 필요
+    path('logout/',LogoutView.as_view(),name='logout'),
+
     path('create/',AccountCreateView.as_view(),name='create'),
 ]

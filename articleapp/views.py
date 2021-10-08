50be048edbf76c902320dcb7577736d152a63b5c
KyeongHwanMin <h45652@naver.com>
2021-10-09 01:17:14 +0900

    refactor(articleapp) : articleapp 구현

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,54 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
-# Create your views here.
+from articleapp.decorators import article_ownership_required
+from articleapp.forms import ArticleCreationForm
+from articleapp.models import Article
+
+
+@method_decorator(login_required, 'post')
+@method_decorator(login_required, 'get')
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreationForm
+    template_name = 'articleapp/create.html'
+
+    def form_valid(self, form): # 유저 설정
+        temp_article = form.save(commit=False)
+        temp_article.writer = self.request.user
+        temp_article.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    context_object_name = 'target_article'
+    template_name = 'articleapp/detail.html'
+
+
+@method_decorator(article_ownership_required, 'post')
+@method_decorator(article_ownership_required, 'get')
+class ArticleUpdateView(UpdateView):
+    model = Article
+    context_object_name = 'target_article'
+    form_class = ArticleCreationForm
+    template_name = 'articleapp/update.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
+
+
+@method_decorator(article_ownership_required, 'post')
+@method_decorator(article_ownership_required, 'get')
+class ArticleDeleteView(DeleteView):
+    model = Article
+    context_object_name = 'target_article'
+    success_url = reverse_lazy('articleapp:list')
+    template_name = 'articleapp/delete.html'

7aa117e2f299b1e3faeb2a08678bbf4efce13022
KyeongHwanMin <h45652@naver.com>
2021-10-08 17:07:20 +0900

    refactor(profileapp) : Profileapp 마무리(이미지출력, 대화명 출력)

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -12,9 +14,19 @@ class ProfileCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
-    def form_valid(self, form): #forms.py에 있는 form을 받음
-        temp_profile = form.save(commit=False) #임시 저장 이므로 commit=False(임시데이터 저장 DB저장 x)
+    def form_valid(self, form):  # forms.py에 있는 form을 받음, 유저 인증을 위함
+        temp_profile = form.save(commit=False)  # 임시 저장 이므로 commit=False(임시데이터 저장 DB저장 x)
         temp_profile.user = self.request.user
         temp_profile.save()
 
         return super().form_valid(form)
+
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'get')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'

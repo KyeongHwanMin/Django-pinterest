4cbc42d9888c5aceb8fcd6151ac5dc7be102ceb4
KyeongHwanMin <h45652@naver.com>
2021-10-11 20:20:09 +0900

    feat(snippets) : ListView, pagination 적용

--- articleapp/views.py
+++ articleapp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
@@ -16,7 +16,7 @@ class ArticleCreateView(CreateView):
     form_class = ArticleCreationForm
     template_name = 'articleapp/create.html'
 
-    def form_valid(self, form): # 유저 설정
+    def form_valid(self, form):  # 유저 설정
         temp_article = form.save(commit=False)
         temp_article.writer = self.request.user
         temp_article.save()
@@ -52,3 +52,10 @@ class ArticleDeleteView(DeleteView):
     context_object_name = 'target_article'
     success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/delete.html'
+
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 5

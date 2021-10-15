6a305dee9bebd82a321747f5eeb1e00d6bd0cc91
KyeongHwanMin <h45652@naver.com>
2021-10-15 14:08:46 +0900

    feat(forms.py) : WYSIWYG 기능 추가

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,14 @@
 from django.forms import ModelForm
-
+from django import forms
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable text-left',
+                                                           'style': 'height: auto;'}))
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False) #프로젝트가 없어도 글을 쓸 수 있게 설정
+
     class Meta:
         model = Article
         fields = ['title', 'image','project', 'content']

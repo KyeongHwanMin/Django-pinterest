c7601825a8ecb732a897450527c423f2fd975bab
KyeongHwanMin <h45652@naver.com>
2021-10-18 17:32:31 +0900

    feat(deploy.py) : docker secret

--- pragmatic/settings/deploy.py
+++ pragmatic/settings/deploy.py
@@ -1,5 +1,16 @@
 from .base import *
 
+
+def read_secret(secret_name):
+
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+
+    return secret
+
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -14,7 +25,7 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False # 개발중인 상태에서 True로 사용
@@ -30,9 +41,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'password1234',
+        'PASSWORD': read_secret('MYSQL_PASSWORD'),
         'HOST': 'mariadb', #마리아 디비 컨테이너 이름
         'PORT': '3306', # mysql,maria db 사용 포트번호
-        
+
     }
 }
